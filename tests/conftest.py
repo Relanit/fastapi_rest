fca201961e5a689d5d900c1ea2e9756bdc6a2c13
Relanit <stulovmaxim3@gmail.com>
2024-10-22 13:58:46 +0300

    refactoring imports

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,13 @@
 from typing import AsyncGenerator, Annotated, TypeVar, get_args, Callable, Awaitable
 
 import pytest
-from fastapi import FastAPI, Depends, params
+from fastapi import Depends, params
 from httpx import AsyncClient, ASGITransport
 from pytest_asyncio import is_async_test
-from sqlalchemy import insert
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 
 from auth.auth import current_user_admin
-from auth.models import User, Role
+from auth.models import User
 from config import config
 from database import get_async_session, Base
 from main import app
@@ -39,14 +38,6 @@ async def prepare_database():
         await conn.run_sync(metadata.drop_all)
 
 
-# @pytest.fixture(autouse=True)
-# async def clear_database(session: AsyncSession):
-#     # Очищаем все таблицы перед каждым тестом
-#     for table in reversed(metadata.sorted_tables):
-#         await session.execute(f"TRUNCATE {table.name} RESTART IDENTITY CASCADE")
-#     await session.commit()
-
-
 @pytest.fixture(scope="session")
 async def client() -> AsyncGenerator[AsyncClient, None]:
     async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as client:
@@ -71,7 +62,6 @@ def get_admin_client():
 
     yield _authenticated_client
 
-    # Remove override
     app.dependency_overrides = {}
 
 
